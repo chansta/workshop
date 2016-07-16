6d59cb662ead27fb6c13003b3914ead846d15347
Felix Chan <fmfchan@gmail.com>
2016-07-16 14:10:42 +0800

    2016.07.12

--- bootstrap/dataobj.py
+++ bootstrap/dataobj.py
@@ -1,1 +1,454 @@
-../../research/python/econometrics/dataobj.py
+############################################################################################################################
+"""
+name: dataobj.py (class)
+created by: Felix chan
+email: fmfchan@gmail.com
+date created: 2012.10.10
+last updated: 2015.04.20 - Added the add_diff function  
+History:	
+	2015.04.20 - Added the add_diff function  
+	2015.04.20 - Added the build_lag_data function
+	2013.12.18 - Replacing print statement with print() function. 
+	2013.12.12 - Updated function MergeRow, including colname and rowname. 
+	2013.08.20
+
+"""
+###########################################################################################################################
+
+import numpy as np
+import scipy as sp 
+import scipy.stats as sps
+import matplotlib.pyplot as plt
+import copy as copy
+#from data import * 
+
+class dataobj(object):
+	"""
+		Create a data object similar to dataframe in R. It uses data.py to import data. 
+	Input:
+		filename: Can either be a filename or a NXT numpy.array
+		sep: column separator in the file
+		skip: number of line to skip before start importing data
+		index: True if the first column contains date in the case of importing data from file. In the case of numpy.array, index can equal a list of date corresponds to the data. 
+		header: True if importing data with variable names in the first row. In the case of numpy.array, header can be a list of variable names.
+	Members:
+		self.data: a NXT numpy.array of data
+		self.N: The number of variables
+		self.T: The number of observations
+		self.header: The list of variable names
+		self.date: A list containing the date information of the data
+		self.mean: Numpy.array of size self.N contaiing the mean of the data
+		self.variance: Numpy.array of size self.N containing the variance of the data
+		self.covariacne: Numpy.array of size self.N X self.N containing the variance-covariance matrix of the date
+		self.correlation: Numpy.array of size self.N X self,.N containing the correlaton matrix of the data
+		
+	"""
+
+	def __init__(self, filename, sep=",", skip=0, index=True, header=True): 
+		if type(filename) == str:
+			allm = gendata(filename, sep=sep, skip=skip, index=index, header=header)
+			if index==True:
+				self.date = allm[0] 
+				self.data = allm[1]
+				self.header = allm[2]
+			else:
+				self.data = allm[0]
+				self.header = allm[1]
+				self.date = "none"
+			self.data = self.data.transpose()
+		else:
+			self.data = filename
+			self.date = index
+			self.header = header
+		self.N,self.T = self.data.shape
+		self.mean = [sps.tmean(i) for i in self.data]
+		if self.N > 1:
+			self.variance = [sps.tvar(i) for i in self.data]
+		else:
+			self.variance = [sps.tvar(self.data)]
+		self.mean = np.array(self.mean)
+		self.variance = np.array(self.variance)
+		self.covariance = np.zeros((self.N, self.N))
+		self.correlation = np.zeros((self.N, self.N))
+		self.skewness = 0
+		self.kurtosis = 0
+		self.dmean = (self.data.transpose()-self.mean).transpose()
+		self.did_covar = False
+		self.JB = 0
+		self.JBpvalue = 0 
+
+	def covar(self, sample=True):
+		"""
+			Calculate the variance-covariance matrix of the given data. 
+			Input:
+				sample: If true then the sample version will be used. 
+			Members Updated:
+				dataobj.covariance
+				dataobj.correlation
+		"""
+		self.covariance = np.dot(self.dmean, self.dmean.transpose())
+		if sample == True:
+			self.covariance = self.covariance/(self.T-1)
+		else:
+			self.covariance = self.covariance/self.T
+		std = [1/pow(j,0.5) for j in np.diag(self.covariance)]
+		std = np.diag(np.array(std))
+		self.correlation = np.dot(np.dot(std,self.covariance), std) 
+		self.did_covar = True
+
+	def __HigherOrder__(self, order=3):
+		"""
+			Sum of data after it is being raised by a power specified in order. 
+			Input:
+				order: 
+			Output:
+				sho: 
+				
+		"""
+		ho = [np.power(j,order) for j in self.dmean]
+		sho = np.array([sum(j) for j in ho])
+		return sho.reshape(self.N)
+
+	def skew(self):
+		"""
+			Calculate sample skewness.
+		"""
+		if self.did_covar==False:
+			self.covar() 
+		self.skewness = self.__HigherOrder__()/((self.T-1)*np.power(np.diag(self.covariance),3/2))
+		return self.skewness
+	
+	def kurt(self):
+		"""
+			Calculate sample kurtosis.
+		"""
+		if self.did_covar==False:
+			self.covar()
+		self.kurtosis = self.__HigherOrder__(order=4)/((self.T-1)*np.power(np.diag(self.covariance),2))
+		return self.kurtosis
+
+	def JBtest(self):
+		"""
+			Calculate the Jarque-Bera test of normality. 
+		"""
+		self.skew()
+		self.kurt()
+		self.JB = self.T*(pow(self.skewness,2)+0.25*pow((self.kurtosis-3),2))/6
+		self.JBpvalue =1-sps.chi2.cdf(self.JB, 2)
+		return self.JB, self.JBpvalue
+	
+	def summary(self, LaTeX=False):
+		"""
+			Print Descriptive Statistics for each variable in the object. 
+		"""
+		if (self.did_covar==False)&(self.N>1):
+			self.covar()
+		self.JBtest()
+		allm = np.c_[self.mean, np.power(self.variance,0.5), self.skew(), self.kurt(), self.JB, self.JBpvalue]
+		lall = [["{0:.8f}".format(i) for i in l] for l in allm]
+		[lall[i].insert(0,"{0:9}".format(self.header[i])) for i in range(0,self.N)]
+		label = ["{0:9}".format("Variables"), "{0:9}".format("Mean"), "Deviation", "Skewness", "Kurtosis", "{0:9}".format("JB Test"), "JB P-value"] 
+		lall.insert(0, label)
+		s = "\n".join(["\t".join([i for i in l]) for l in lall])
+		print("The number of variable(s) is {0}".format(self.N))
+		print("The number of observations is {0}\n".format(self.T))
+		print(s)
+		if self.N>1:
+			print("\nThe Correlation Matrix is")
+			print(self.correlation)
+			print("\nThe variance-covariance matrix is ")
+			print(self.covariance)
+		if LaTeX!=False:
+			lall = [["{0:.3f}".format(i) for i in l] for l in allm]
+			[lall[i].insert(0,"{0:9}".format(self.header[i])) for i in range(0,self.N)]
+			lall.insert(0, label)
+			s = "\\\\ \n".join(["&".join([i for i in l]) for l in lall])
+			beginning = "\\begin{tabular}{c|cccccc|} \n \\hline \n"
+			end = "\\\\ \n \\hline \\end{tabular} \n"
+			alls = beginning + s + end
+			fout = open(LaTeX, "w")
+			fout.write(alls)
+			fout.close()
+	
+	def showgraph(self, s=[2,2]):
+		plt.rcParams["font.size"]=10
+		if self.data != "none":
+			x = np.linspace(0,self.T-1, 5)
+			xloc = [int(np.floor(i)) for i in x]
+			xlabel = [self.date[i] for i in xloc]
+		j = 1
+		for i in self.data:
+			plt.subplot(s[0], s[1], j)
+			plt.plot(i)
+			if len(self.header)>0:
+				plt.title(self.header[j-1])
+			if self.date != "none":
+				plt.xticks(xloc, xlabel)
+			j = j + 1
+		plt.show()
+	
+	def extract_var(self, var):
+		"""
+			Extract the data for variables listed in var. 
+			Input:
+				var: a list containing names of variable to be extracted. 
+			Output:
+				newobj: a dataobj object containing the data for the variables as listed in var
+		"""
+		if all([i in self.header for i in var])==False:
+			print("Not all the variables are in the dataset.")
+			return 0
+		else: 
+			indexset = [self.header.index(i) for i in var]
+			X = self.data[indexset]
+			return dataobj(X, index=self.date, header=var)
+				
+def gendata(filename, sep=",", skip=0, index=True, header=True):
+	"""
+	Import data from a text file. If index is true then the first column will be treated as an identifier.
+	Input:
+		filename: The name of the file containing the data 
+		sep: The separator used to separate columns
+		skip: The number of line to skip before importing the first row of data
+		index: True if the first column contains date information
+		header: True if the first row contains the name of the variables. 
+	Output:
+		return datef, m, headerinfo if index is true. 
+		return m, headerinfo if index is false. 
+		In the case when header is false, headerinfo is an empty list. 
+		
+	"""
+	f = open(filename)
+	m = []
+	headerinfo = []
+	if index == True:
+		datef = []
+	for i,line in enumerate(f):
+		if i >= skip:
+			source = line.split(sep)
+			n = len(source)
+			source[n-1] = source[n-1].rstrip()
+			if (i==skip)&(header==True):
+				if index==True:
+					headerinfo = source[1:n]
+				else:
+					headerinfo = source
+			else:
+				if index==True:
+					temp = np.array([float(i) for i in source[1:n]])
+					datef.append(source[0])
+				else: 
+					temp = np.array([float(i) for i in source[0:n]])
+				m.append(temp)
+	f.close()
+	#m.pop()
+	m = np.array(m)
+	T,N = m.shape
+	if len(headerinfo)==0:
+		headerinfo = ["v"+str(i+1) for i in range(0,N)]
+	if index==True:
+	#	datef.pop()
+		return datef,m, headerinfo
+	else:
+		return m, headerinfo
+
+def vec_data(filename, sep=",", sortby="time", savefilename=False):
+	"""
+		Convert a rectangular matrix of data into column. Assuming the first row contains names and the first column contains date
+	"""
+	f = open(filename)
+	dateinfo = []
+	data = []
+	for i,line in enumerate(f):
+		if i==0:
+			names = line.split(sep)
+			names = names[1:len(names)]
+			names[len(names)-1]=names[len(names)-1].rstrip()
+			N = len(names)
+		else:
+			temp = line.split(sep)
+			temp[len(temp)-1].rstrip()
+			dateinfo.append(temp[0])
+			data.append([float(j) for j in temp[1:len(temp)]])
+	f.close()
+	T = len(dateinfo)
+	data = np.array(data).transpose().reshape(N*T)	
+	if sortby == "time":
+		dateinfocol = dateinfo*N
+		namescol = []
+		for i in range(0,N):
+			namescol = namescol + [names[i]]*T
+	else:
+		namescol = names*T
+		dateinfocol = []
+		for i in range(0,T):
+			dateinfocol = dateinfocol+[dateinfo[i]]*N
+	if savefilename != False: 
+		m = [[dateinfocol[i], namescol[i], str(data[i])] for i in range(0,T*N)]
+		s = "\n".join([",".join([j for j in l]) for l in m])
+		f = open(savefilename,"w")
+		f.write(s)
+		f.close()
+	return dateinfocol, namescol, data
+
+def writeRdata(filename, m, sep=",", header=0, do_transpose=True):
+	"""
+	Export the numpy array m to a file, filename. Variable names can be included in header. 
+	"""
+	if do_transpose == True:
+		s = "\n".join([sep.join([str(j) for j in i]) for i in m.transpose() ])
+	else:
+		s = "\n".join([sep.join([str(j) for j in i]) for i in m])
+	if header!=0:
+		head = sep.join([i for i in header])+"\n" 
+		s = head+s
+	f = open(filename, "w")
+	f.write(s)
+	f.close()
+
+def MergeRow(savefilename, m1, m2, bracket=["(", ")"], colname=None, rowname=None):
+	"""
+	Merge two arrays m1 and m2. The first row in m2 will appear under the first rwo from m1 with a bracket added. Its output will be saved to savefilename. 
+	"""
+	
+	N,T=m1.shape
+	m = []
+	if (T>=2):
+		x = [["{0:.3f}".format(j) for j in l] for l in m1]
+		y  = [[bracket[0]+"{0:.3f}".format(j)+bracket[1] for j in l] for l in m2]
+		
+	else:
+		x = ["{0:.3f}".format(j) for j in m1]
+		y  = [bracket[0]+"{0:.3f}".format(j)+bracket[1] for j in m2]
+	for i in range(0,N):
+		if rowname is None: 
+			m.append(x[i])
+			m.append(y[i])
+		else:
+			m.append([rowname[i]]+x[i])
+			m.append(list(" ") + y[i])
+	if (T>=2):
+		s = "\n".join([",".join([i for i in l]) for l in m])
+	else:
+		s = "\n".join([i for i in m])
+	if colname is not None: 
+		header = ","+",".join([l for l in colname])+"\n"
+		s = header + s
+	f=open(savefilename,"w")
+	f.write(s)
+	f.close()
+
+def differenced(dob,order=1, transform='log'):
+	"""
+	Create a dataobj from dob which contianed the differenced varaibles with transformed specified in transform.
+	"""
+	tempobj = copy.copy(dob)
+	if transform == 'log':
+		tempobj.data = np.log(tempobj.data).transpose()
+	tempobj.data = tempobj.data[order:tempobj.T]-tempobj.data[0:tempobj.T-order]
+	tempobj.date = tempobj.date[order:tempobj.T]
+	newobj = dataobj(tempobj.data.transpose(), index=tempobj.date, header=tempobj.header)
+	return newobj
+
+def format_table(filename, savefilename, model=False, bracket=["(", ")"], LaTeX=True):
+	"""
+		Filename contains even number columns N with the first N/2 columns containing coefficient estiamtes and the last N/2 columns contains the corresponding stnadard errors. The routine will put the standard errors under the associated coefficients with the bracket specified in the argument bracket.
+	"""
+	if type(filename) == str:
+		m = dataobj(filename)
+	elif type(filename).__name__ != "dataobj":
+		print("File {0} is not being supported.".format(filename))
+	N,nvar = m.data.shape
+	nmodel = N/2 
+	coef = m.data[0:nmodel]
+	std = m.data[nmodel:N] 
+	coefs = [["{0:.3f}".format(i) for i in l] for l in coef.transpose()]
+	stds = [["{0:.3f}".format(i) for i in l] for l in std.transpose()]
+	s = ""
+	if LaTeX==False:
+		if model != False:
+			s = "&" + "&".join(model)+" \n"
+		for i in range(0,nvar):
+			s = s+m.date[i]+"&"+"&".join([j for j in coefs[i]]) + "\n" + "&"+"&".join([bracket[0]+k+bracket[1] for k in stds[i]])+"\n"
+	else: 
+		for i in range(0,nvar):
+			s = s+m.date[i]+"&"+"&".join([j for j in coefs[i]]) + "\\\\\n" + "&"+"&".join([bracket[0]+k+bracket[1] for k in stds[i]])+"\\\\\n"
+		header = "\\begin{tabular}{c|"
+		for i in range(0,nmodel):
+			header = header + "c"
+		header= header + "|} \n \\hline"
+		if model != False:
+			header = header + "&" + "&".join(model)+"\\\\ \\hline \n"
+		end = "\hline\\end{tabular}"
+		s = header + s + end
+	f = open(savefilename, "w")
+	f.write(s)
+	f.close()
+def add_lag(obj, lag=1):
+	"""
+	Create a dataobject from obj with lag variables. 
+	Input: 
+		obj: a dataobj object
+		lag: how many lag to add. Default is 1. 
+	Output:
+		An dataobj with the same variables as obj as well as their lag values.
+	"""
+	if lag > obj.T:
+		s = "You don't have enough obervsations. The number of observations is {0} and the number of lag is {1}.".format(obj.T, lag)
+		print(s)
+	data = copy.copy(obj.data)
+	cdata = data.transpose()[lag:obj.T].transpose()
+	header = copy.copy(obj.header)
+	for i in range(1,lag+1):
+		temp = data.transpose()[lag-i:obj.T-i].transpose()
+		cdata = np.r_[cdata,temp]
+		header = header + [j+"("+str(-i)+")" for j in obj.header] 
+	newobj = dataobj(filename=cdata, header = header, index=obj.date[lag:obj.T])
+	return newobj
+
+def add_diff(obj, diff=1):
+	"""
+	Create a dataobject from obj with differenced varaibles based on the lag as specified in diff. 
+	Input:
+		obj: a dataobj object. 
+		diff: the lag for differences X.ddiff = X-X(-diff).
+	Output:
+		An dataobj with the same variables as obj as well as the differenced variables. The differenced variable will be labelled as X.ddiff unless diff=1 in which case it will be X.d with X being the name of the original variable. The values of X.ddiff for the first diff obervations will be 0.  
+	"""
+	data = copy.copy(obj.data)
+	cdata = data.transpose()[diff:obj.T] - data.transpose()[0:obj.T-diff]
+	cdata = np.c_[np.zeros((obj.N,diff)), cdata.transpose()]
+	cdata = np.r_[data, cdata]
+	if diff == 1:
+		appendix = ".d"
+	else:
+		appendix = ".d"+str(diff)
+	header = obj.header+[i+appendix for i in obj.header]
+	newobj = dataobj(filename=cdata, index=obj.date, header=header)
+	return newobj	
+
+def build_lag_data(m, lag=1):
+    """
+    Build a dataframe to contain the data for all variables as well as their lags with time period adjusted. Generalisatio of add_lag
+    Input: 
+    m: a dataobj object. 
+    lag: The number of lag to be considered. 
+    Output:
+    est_dataobj: A dataobject containing all the variables from m as well as their lags. It will lose lag number of observations due to lagging. The lagged variables will have (-i) attached to its name. For example, the lag of X will be labeled as X(-1).      
+    """
+    mainX = m.data.transpose()[lag:m.T] 
+    header = copy.copy(m.header)
+    for i in range(1,lag+1):
+        temp = m.data.transpose()[lag-i:m.T-i]
+        temp_header = [j+"("+str(-i)+")" for j in m.header]
+    mainX = np.c_[mainX, temp]
+    header = header + temp_header
+    est_data = mainX
+    est_header = header
+    est_dataobj =dataobj(est_data.transpose(), index=m.date[lag:m.T], header=est_header) 
+    return est_dataobj
+
+
+
+
+
--- bootstrap/lm.py
+++ bootstrap/lm.py
@@ -1,1 +1,212 @@
-../../research/python/econometrics/lm.py
+############################################################################################################################
+"""
+Name: lm.py (class)
+Author: Felix Chan
+Email: fmfchan@gmail.com
+Date Created: 2012.10.10
+Last Updated:	2013.12.28 - Replacing print statement with print() function 
+History:	2013.01.13
+
+"""
+##########################################################################################################################
+import numpy as np
+import scipy as sp
+import scipy.stats as sps
+import matplotlib.pyplot as plt
+import dataobj as dob
+
+class lm:
+	"""
+	A collection of routines for estimating linear regression models. 
+	Note: This is not finished yet. Only OLS under the classical assumption has been implemented. 
+	Input:
+		formula: In a format similar to R. It takes advantage of variable names if data is a data object as defined in dataobj (see data below). Otherwise, if data is numpy.ndarray, then the variable names should be the column index of the data matrix. For example, "1~c+2+3" means regressing the first column data against an intercept "c", the second and third column data. Alternatively, "Y~c+X+Z" means regressing the variable "Y" on an intercept, the variable X and the variable Z. These variable names must be in dataobj.header in the latter case. The absence of "c" means there is no intercept in the model.  
+		data: Either a dataobj or TXN numpy.ndarray, where T is the number of observations and N is the number of variates. 
+		header: If data is a numpy.ndarray then header can contain a list of variable names corresponds to the formula. 
+		cov_method: "ols", calculated under the classical assumption; "residuals", calculated by taking the average of residuals. 
+	Members:
+		self.coef: the parameter estimates following the specificaion of the formula.
+		self.cov: the variance-covariance matrix of self.coef
+		self.sterr: the standard errors of self.coef
+		self.tstats: the t-statistics of self.coef
+		self.p: the p-values associated with the t-statistics
+		self.sigma: the variance of the estimated errors
+		self.omega: the variance-covariance matrix of residuals. Users must provide self.omega in the case of gls
+		self.invOmega: the inverse of self.omega
+		self.RSS: Residuals sum of squares
+		self.MSS: Model sum of squares
+		self.TSS: Total sum of squares
+		self.fitted: fitted values of the dependent variable
+		self.residuals: residuals from the fitted model 
+		self.R2: R squared
+		self.adjR2: Adjusted R squared
+		self.F: F test statistics for regression
+		self.pF: The p-value associated with the F test statistics 
+		self.k: the number of parameters estiamted 	
+		self.N: the number of variates in the data
+		self.T: the number of observations
+	"""
+
+	def __init__(self, formula, data, header="default"):
+
+		dformula = formula.split("~")
+		self.formula = formula
+		self.depvar = dformula[0] 
+		self.explvar = dformula[1].split("+") #Deconstructing the formula: assigning dependent and explanatory variables. 
+		if 'c' in self.explvar: #determine if there is an intercept in the model.
+			self.explvar.remove('c') 
+			self.intercept = True
+		else:
+			self.intercept = False
+		if type(data)==np.ndarray: #Constructing the Y and X matricies based on the nature of "data"
+			self.T, self.N = data.shape
+			self.data = data.transpose()
+			if header == "default":
+				getindex = [int(i) for i in self.explvar]
+				self.Y = self.data[int(self.depvar)]
+				self.explvar = ["v"+str(i) for i in range(1,self.N+1)]
+				self.depvar = "Y"
+			else:
+				self.header = header
+				getindex = [self.header.index(i) for i in self.explvar]
+				self.Y = self.data[self.header.index(self.depvar)]
+
+		else:
+			self.data = data.data
+			self.N, self.T = self.data.shape
+			self.header = data.header
+			getindex = [self.header.index(i) for i in self.explvar]
+			self.Y = self.data[self.header.index(self.depvar)]
+		self.X = np.array([self.data[i] for i in getindex])
+		if self.intercept == True:
+			self.X = np.r_[np.ones((1,self.T)), self.X] 
+			self.explvar.insert(0,"Intercept")
+		self.estchoice = "ols" #setting the default estimator to ols. 
+		self.X = self.X.transpose()
+		self.Y = self.Y.reshape((self.T,1))
+		self.k = self.X.shape[1]
+		self.omega = np.eye(self.k) #setting the default covariance matrix for GLS as identity matrix.
+		self.invOmega = np.linalg.inv(self.omega)
+		self.residuals = 0
+		self.fitted = 0
+		self.coef = 0
+		self.cov = 0
+		self.sigma = 0
+		self.sterr = 0
+		self.tstats = 0
+		self.p = 0
+		self.RSS = 0 #Residuals sum of squares
+		self.MSS = 0 #Model (Estiamted) sum of squares
+		self.TSS = 0 #Totla sum of squares
+		self.got_residuals = False
+
+	def __ols__(self):
+		"""
+			Get the OLS estimator
+		"""
+		if self.estchoice == "ols" or self.estchoice == "ols_g":
+			self.coef=np.dot(np.linalg.inv(np.dot(self.X.transpose(), self.X)), np.dot(self.X.transpose(), self.Y))
+		elif self.estchoice == "gls": 
+			wX = np.dot(self.invOmega, self.X) 
+			wY = np.dot(self.invOmega, self.Y)
+			self.coef=np.dot(np.linalg.inv(np.dot(self.X.transpose(), wX)), np.dot(self.X.transpose(), wY))
+		else:
+			print("The estimator has not been implemented yet.")
+
+	def __residuals__(self):
+		"""
+			Calculate residuals and fitted values
+		"""
+		self.fitted = np.dot(self.X, self.coef)
+		self.residuals = self.Y - self.fitted 
+		self.got_residuals = True
+
+	def __anova__(self):
+		"""
+			Partially finished anova table. Though it does provide R^2 and the variance-covariance matrix.
+		"""
+		if self.got_residuals == False:
+			self.__residuals__()
+		dy = self.Y - sp.stats.tmean(self.Y)
+		dfy = self.fitted - sp.stats.tmean(self.Y)
+		RSS = float(np.dot(self.residuals.transpose(), self.residuals).reshape(1))
+		MSS = float(np.dot(dfy.transpose(),dfy).reshape(1))
+		TSS = float(np.dot(dy.transpose(),dy).reshape(1))
+		self.sigma = RSS/float(self.T-self.k)
+		if self.estchoice == "ols":
+			self.cov = self.sigma*np.linalg.inv(np.dot(self.X.transpose(), self.X))
+		elif self.estchoice == "ols_g":
+			invXX = np.dot(self.X.transpose(), self.X)
+			XOX = np.dot(self.X.transpose(), np.dot(self.omega, self.X))
+			self.cov = np.dot(invXX, np.dot(XOX, invXX)) 
+		elif self.estchoice == "gls":
+			temp = np.dot(self.X.transpose(), np.dot(self.invOmega, self.X))
+			self.cov = np.linalg.inv(temp)
+		else: 
+			print("The variance estimator has not been implemented yet.")
+		self.R2 = 1-RSS/TSS
+		self.adjR2 = 1- (1-self.R2)*((self.T-1)/float(self.T-self.k))
+		self.F = MSS/RSS*((self.T-self.k)/float(self.k-1))
+		self.pF = 1-sps.f.cdf(self.F,self.k-1, self.T-self.k)
+		self.RSS = RSS
+		self.TSS = TSS
+		self.MSS = MSS 
+
+	def __sterr__(self):
+		"""
+			Calculate the standard errors, t statistics and p-values
+		"""
+		self.sterr = np.power(np.diag(self.cov),0.5)
+		self.tstats = self.coef.reshape(self.k)/self.sterr
+		self.p = 1-sp.stats.t.cdf(abs(self.tstats),self.T-self.k)
+	
+	def estimate(self, estimator="ols", cov=1):
+		"""
+			Users interface to estimate the specified model
+			estimtor options: ols, ols_g, gls
+		"""
+		if estimator == "ols" or estimator =="ols_g":
+			self.estchoice = estimator 
+		elif estimator == "gls":
+			if type(cov)==int:
+				print("Please enter the covariance matrix to be used with GLS.")
+			else:
+				self.omega = cov
+				self.invOmega = np.linalg.inv(cov)
+				self.estchoice = estimator
+		self.__ols__()
+		self.__residuals__()
+		if estimator == "ols_g":
+			e = self.residuals
+		self.__anova__()
+		self.__sterr__()
+		self.got_estimates = True
+	
+	def summary(self, showgraph=False):
+		"""
+			Present output
+		"""
+		col_label = ["Variables", "Coefficients", "Std. Errors", "t-statistics", "p-values"] 
+		stats = np.c_[self.coef.reshape(self.k), self.sterr.reshape(self.k), self.tstats.reshape(self.k), self.p.reshape(self.k)]
+		allm= [["{0:8.4g}".format(i) for i in l] for l in stats]
+		[allm[i].insert(0, "{0:12}".format(self.explvar[i])) for i in range(0,self.k)]
+		allm.insert(0,col_label)
+		s = "\n".join(["\t".join([i for i in l]) for l in allm])
+		print("The regression output for the model {0}".format(self.formula))
+		print("The number of observation is {0}".format(self.T))
+		print("The number of estimates is {0}".format(self.k))
+		print("The dependent variable is {0}".format(self.depvar))
+		print(s)
+		print("The R^2 is {0}".format(self.R2))
+		print("The adjusted R^2 is {0}.".format(self.adjR2))
+		print("The F test statistics is {0} with p value {1}.".format(self.F, self.pF))
+		if showgraph==True:
+			plt.subplot(2,1,1)
+			plt.plot(self.Y)
+			plt.plot(self.fitted, "r-")
+			plt.title("Data and Fitted Values")
+			plt.legend(("{0}".format(self.depvar), "Fitted Values"))
+			plt.subplot(2,1,2)
+			plt.plot(self.residuals)
+			plt.title("Residuals of {0}".format(self.depvar))
+			plt.show()
